e1501b99eb4478743db480f9d51d01964910c8fd
Ashharahmed <69009874+Ashharahmed@users.noreply.github.com>
2021-02-03 00:01:45 +1100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "empdatabase-12.cwzcccaqrmsz.us-east-1.rds.amazonaws.com"
 customuser = "Ashman"
 custompass = "Ashman97"
-customdb = "empdatabase-12"
+customdb = "employees"
 custombucket = "employeesadd"
 customregion = "us-east-1"
